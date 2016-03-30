b1a63c6e0a297b173d00f6efeacf669d204fbeef
Michael Lawrence <m6lawrence1@gmail.com>
2016-03-29 20:50:32 -0400

    Initial commit

---
+++ lab4_tempConvFunc/tempConvert.js
@@ -0,0 +1,17 @@
+
+
+var num = 50;
+var cf = c
+
+ var cel = function convertTemp (num, cf) {
+ 	if (cf === c) {
+ 	convert = num * 9/5;
+ 	}
+ 	else if (cf === f) {
+ 	convert = (num-32)*5/9; 
+ 	}
+ 	else {
+ 		console.log("Please enter either C or F")
+ 	}
+
+ 	convertTemp();
